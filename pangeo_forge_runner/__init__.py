364189a633d2733f39ff6373c58f7be556c700f3
YuviPanda <yuvipanda@gmail.com>
2022-07-27 16:56:32 -0700

    Add functionality to expand meta.yaml if needed

--- pangeo_forge_runner/__init__.py
+++ pangeo_forge_runner/__init__.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from ruamel.yaml import YAML
+from copy import deepcopy
 
 yaml = YAML()
 
@@ -30,6 +31,11 @@ class Feedstock:
         return self._import_cache[module][export]
 
     def parse_recipes(self):
+        """
+        Parse the recipes defined in this feedstock & return them
+
+        *Executes arbitrary code* defined in the feedstock!!!
+        """
         recipes = {}
         recipes_config = self.meta['recipes']
         if isinstance(recipes_config, list):
@@ -41,3 +47,23 @@ class Feedstock:
             raise ValueError(f'Could not parse recipes config in meta.yaml')
 
         return recipes
+
+    def get_expanded_meta(self):
+        """
+        Return full meta.yaml file, expanding recipes if needed.
+
+        recipes are guaranteed to be a list of dictionaries with 'id' keys.
+        'object' keys *may* be present, but not guaranteed.
+        """
+        meta_copy = deepcopy(self.meta)
+        print(self.meta)
+        if 'recipes' in self.meta and 'dict_object' in self.meta['recipes']:
+            do = self.meta['recipes']['dict_object']
+            # We have a dict_object, so let's parse the recipes, and provide
+            # keep just the ids, discarding the values.
+            recipes = self.parse_recipes()
+            meta_copy['recipes'] = [{'id': k} for k, v in recipes.items()]
+
+        return meta_copy
+
+
--- tests/test_recipes.py
+++ tests/test_recipes.py
@@ -1,23 +1,36 @@
 from pangeo_forge_runner import Feedstock
 from pathlib import Path
+from ruamel.yaml import YAML
+
+yaml = YAML()
 
 HERE = Path(__file__).parent
 
 def test_recipes_list():
     list_recipe = HERE / 'test-recipes/list-recipes'
-    f = Feedstock(list_recipe)
-    recipes = f.parse_recipes()
+    feed = Feedstock(list_recipe)
+    recipes = feed.parse_recipes()
     assert recipes == {
         'test_1': 'test_1',
         'test_2': 'test_2'
     }
+
+    with open(list_recipe / 'feedstock/meta.yaml') as f:
+        meta = yaml.load(f)
+        assert meta == feed.get_expanded_meta()
+
 
 
 def test_recipes_dict():
-    list_recipe = HERE / 'test-recipes/list-recipes'
-    f = Feedstock(list_recipe)
-    recipes = f.parse_recipes()
+    list_recipe = HERE / 'test-recipes/dict-recipes'
+    feed = Feedstock(list_recipe)
+    recipes = feed.parse_recipes()
     assert recipes == {
         'test_1': 'test_1',
         'test_2': 'test_2'
     }
+
+    with open(list_recipe / 'feedstock/meta.yaml') as f:
+        meta = yaml.load(f)
+        meta['recipes'] = [{'id': 'test_1'}, {'id': 'test_2'}]
+        assert meta == feed.get_expanded_meta()
