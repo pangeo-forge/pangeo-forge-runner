2b452ab5674baf9d6b4a2c6ea36a35cca946936f
YuviPanda <yuvipanda@gmail.com>
2022-07-27 17:50:13 -0700

    Move required files to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ setup(
     name='pangeo-forge-runner',
     version='0.1',
     packages=find_packages(),
+    install_requires=[
+        'jupyter-repo2docker',
+        'ruamel.yaml'
+    ],
     entry_points={
         'console_scripts': ['pangeo-forge-runner=pangeo_forge_runner.__main__:main']
     }
