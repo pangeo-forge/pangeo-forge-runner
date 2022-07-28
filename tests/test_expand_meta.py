a39f54985a4a92af9001b697f817a657c713b14c
YuviPanda <yuvipanda@gmail.com>
2022-07-27 18:14:14 -0700

    Update test fixture for CMIP6 data

--- tests/test_expand_meta.py
+++ tests/test_expand_meta.py
@@ -7,41 +7,7 @@ invocations = [
     {
         "repo": "https://github.com/pangeo-forge/cmip6-feedstock",
         "ref": "63f544892df0f6c7c61bf372bfd52366cd885aa7",
-        "meta": {
-            "title": "CMIP6",
-            "description": "CMIP6 datasets converted to zarr stores from ESGF files",
-            "pangeo_forge_version": "0.8.3",
-            "pangeo_notebook_version": "2021.12.02",
-            "recipes": [
-                {
-                    "id": "CMIP6.PMIP.MPI-M.MPI-ESM1-2-LR.past2k.r1i1p1f1.Amon.tas.gn.v20210714"
-                }
-            ],
-            "provenance": {
-                "providers": [
-                    {
-                        "name": "ESGF",
-                        "description": "Earth System Grid Federation",
-                        "roles": ["producer", "licensor"],
-                        "url": "https://esgf-node.llnl.gov/projects/cmip6/",
-                    }
-                ],
-                "license": "CC-BY-4.0",
-            },
-            "maintainers": [
-                {
-                    "name": "Julius Busecke",
-                    "orcid": "0000-0001-8571-865X",
-                    "github": "jbusecke",
-                },
-                {
-                    "name": "Charles Stern",
-                    "orcid": "0000-0002-4078-0852",
-                    "github": "cisaacstern",
-                },
-            ],
-            "bakery": {"id": "pangeo-ldeo-nsf-earthcube"},
-        },
+        "meta": {"title": "CMIP6", "description": "CMIP6 datasets converted to zarr stores from ESGF files", "pangeo_forge_version": "0.8.3", "pangeo_notebook_version": "2021.12.02", "recipes": [{"id": "CMIP6.PMIP.MIROC.MIROC-ES2L.past1000.r1i1p1f2.Amon.tas.gn.v20200318"}, {"id": "CMIP6.PMIP.MRI.MRI-ESM2-0.past1000.r1i1p1f1.Amon.tas.gn.v20200120"}, {"id": "CMIP6.PMIP.MPI-M.MPI-ESM1-2-LR.past2k.r1i1p1f1.Amon.tas.gn.v20210714"}], "provenance": {"providers": [{"name": "ESGF", "description": "Earth System Grid Federation", "roles": ["producer", "licensor"], "url": "https://esgf-node.llnl.gov/projects/cmip6/"}], "license": "CC-BY-4.0"}, "maintainers": [{"name": "Julius Busecke", "orcid": "0000-0001-8571-865X", "github": "jbusecke"}, {"name": "Charles Stern", "orcid": "0000-0002-4078-0852", "github": "cisaacstern"}], "bakery": {"id": "pangeo-ldeo-nsf-earthcube"}}
     },
     {
         "repo": "https://github.com/pangeo-forge/gpcp-feedstock",
