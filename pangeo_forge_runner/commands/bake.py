c392a5d50d90f6a4375ec61a63f83b87f3566cf5
YuviPanda <yuvipanda@gmail.com>
2022-08-02 14:43:34 -0700

    Add slightly more logging

--- pangeo_forge_runner/commands/bake.py
+++ pangeo_forge_runner/commands/bake.py
@@ -46,10 +46,20 @@ class Bake(BaseCommand):
     )
 
     def start(self):
+
+        target_storage = TargetStorage(parent=self)
+        input_cache_storage = InputCacheStorage(parent=self)
+        metadata_cache_storage = MetadataCacheStorage(parent=self)
+
+        self.log.info(f'Target Storage is {target_storage}\n', extra={'status': 'setup'})
+        self.log.info(f'Input Cache Storage is {input_cache_storage}\n', extra={'status': 'setup'})
+        self.log.info(f'Metadata Cache Storage is {metadata_cache_storage}\n', extra={'status': 'setup'})
+
         with tempfile.TemporaryDirectory() as d:
             self.fetch(d)
             feedstock = Feedstock(Path(d))
-            self.log.info("Parsing recipes", extra={'status': 'running'})
+
+            self.log.info("Parsing recipes...", extra={'status': 'running'})
             with redirect_stderr(self.log, {'status': 'running'}), redirect_stdout(self.log, {'status': 'running'}):
                 recipes = feedstock.parse_recipes()
 
@@ -62,17 +72,17 @@ class Bake(BaseCommand):
             )
 
 
-            target_storage = TargetStorage(parent=self)
-            input_cache_storage = InputCacheStorage(parent=self)
-            metadata_cache_storage = MetadataCacheStorage(parent=self)
 
             for name, recipe in recipes.items():
                 job_name=f'{name}-{recipe.sha256().hex()}-{int(datetime.now().timestamp())}'
+
                 recipe.storage_config = StorageConfig(
-                    target_storage.get_forge_target(job_id=job_name),
-                    input_cache_storage.get_forge_target(job_id=job_name),
-                    metadata_cache_storage.get_forge_target(job_id=job_name)
+                    target_storage.get_forge_target(job_name=job_name),
+                    input_cache_storage.get_forge_target(job_name=job_name),
+                    metadata_cache_storage.get_forge_target(job_name=job_name)
                 )
+
+
                 pipeline_options = bakery.get_pipeline_options(
                     # FIXME: Put in repo / ref here
                     job_name=job_name,
--- pangeo_forge_runner/storage.py
+++ pangeo_forge_runner/storage.py
@@ -29,7 +29,10 @@ class StorageTargetConfig(LoggingConfigurable):
         "",
         config=True,
         help="""
-        Root path under which to put all our storage
+        Root path under which to put all our storage.
+
+        If {job_name} is present in the root_path, it will be expanded to the
+        unique job_name of the current job.
         """
     )
 
@@ -40,14 +43,21 @@ class StorageTargetConfig(LoggingConfigurable):
         """
     )
 
-    def get_forge_target(self, job_id):
+    def get_forge_target(self, job_name: str):
         """
         Return correct pangeo-forge-recipes Target
+
+        If {job_name} is present in `root_path`, it is expanded with the given job_name
         """
         return self.pangeo_forge_target_class(
             self.fsspec_class(**self.fsspec_args),
-            root_path=self.root_path.format(job_id=job_id)
+            root_path=self.root_path.format(job_id=job_name)
         )
+
+    def __str__(self):
+        # Only show keys and type of values of args to fsspec, as they might contain secrets
+        fsspec_args_filtered = ', '.join(f'{k}=<{type(v).__name__}>' for k, v in self.fsspec_args.items())
+        return f'{self.pangeo_forge_target_class.__name__}({self.fsspec_class.__name__}({fsspec_args_filtered}, root_path="{self.root_path}")'
 
 
 class TargetStorage(StorageTargetConfig):
