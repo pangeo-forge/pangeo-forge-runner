0ef04eb3ae7ee68543c088504ccfe4c888b3705d
YuviPanda <yuvipanda@gmail.com>
2022-08-03 11:15:54 -0700

    Add unit test for dataflow pipelineoptions

--- tests/test_dataflow.py
+++ tests/test_dataflow.py
@@ -38,3 +38,29 @@ def test_temp_gcs_location_validation():
         dfb.temp_gcs_location = 'This Should be an Error'
 
     dfb.temp_gcs_location = 'gs://this-should-not-error'
+
+
+def test_pipelineoptions():
+    """
+    Quickly validate some of the PipelineOptions set
+    """
+    dfb = DataflowBakery()
+    dfb.project_id = "hello"
+    dfb.region = "us-west1"
+    dfb.machine_type = "n1-standard-2"
+    dfb.use_public_ips = True
+    dfb.temp_gcs_location = 'gs://something'
+
+    po = dfb.get_pipeline_options('job', 'some-container:some-tag')
+    opts = po.get_all_options()
+    assert opts['project'] == "hello"
+    assert opts['use_public_ips']
+    assert opts["temp_location"] == "gs://something"
+    assert opts["machine_type"] == "n1-standard-2"
+    assert opts["region"] == "us-west1"
+    assert opts["experiments"] == ["use_runner_v2"]
+    assert opts["save_main_session"]
+    assert opts["pickle_library"] == "cloudpickle"
+    assert opts["sdk_container_image"] == "some-container:some-tag"
+    assert opts["job_name"] == "job"
+    assert opts["runner"] == "DataflowRunner"
