59ee24f87a4c36c78282c53b38461e5b887e5be2
YuviPanda <yuvipanda@gmail.com>
2022-12-28 14:11:46 -0800

    Add simple prune test for RecipeRewriter

---
+++ tests/rewriter-tests/prune/rewritten.py
@@ -0,0 +1,6 @@
+import apache_beam as beam
+from pangeo_forge_recipes.patterns import pattern_from_file_sequence
+
+pattern = pattern_from_file_sequence(["hi.nc"])
+
+recipe = beam.Create(pattern.prune().items())
