3f6ad09dba561bee2e38b099771c0821fae4f3a7
YuviPanda <yuvipanda@gmail.com>
2023-02-14 14:33:56 -0800

    Use job_name, not job_id in target specs
    job_id is only known *after* job submission, and can not
    be used for expansion

--- tests/integration/test_dataflow_integration.py
+++ tests/integration/test_dataflow_integration.py
@@ -18,15 +18,15 @@ def test_dataflow_integration():
         "DataflowBakery": {"temp_gcs_location": bucket + "/temp"},
         "TargetStorage": {
             "fsspec_class": "gcsfs.GCSFileSystem",
-            "root_path": bucket + "/target/{job_id}",
+            "root_path": bucket + "/target/{job_name}",
         },
         "InputCacheStorage": {
             "fsspec_class": "gcsfs.GCSFileSystem",
-            "root_path": bucket + "/input-cache/{job_id}",
+            "root_path": bucket + "/input-cache/{job_name}",
         },
         "MetadataCacheStorage": {
             "fsspec_class": "gcsfs.GCSFileSystem",
-            "root_path": bucket + "/metadata-cache/{job_id}",
+            "root_path": bucket + "/metadata-cache/{job_name}",
         },
     }
 
