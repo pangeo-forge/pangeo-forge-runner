c8b6844f3f520ecadba15b08ed63161ee9933554
YuviPanda <yuvipanda@gmail.com>
2023-08-19 11:24:35 -0700

    Throw error and exit if config file isn't readable
    Fixes https://github.com/pangeo-forge/pangeo-forge-runner/issues/73

--- pangeo_forge_runner/commands/base.py
+++ pangeo_forge_runner/commands/base.py
@@ -211,8 +211,17 @@ class BaseCommand(Application):
 
     def initialize(self, argv=None):
         super().initialize(argv)
-        # Load traitlets config from a config file if present
-        self.load_config_file(self.config_file)
+        # Load traitlets config from a config file if passed
+        if self.config_file:
+            if os.path.exists(self.config_file):
+                # Throw an explicit error and exit if config file isn't present
+                print(
+                    f"Could not read config from file {self.config_file}. Make sure it exists and is readable",
+                    file=sys.stderr,
+                )
+                sys.exit(1)
+
+            self.load_config_file(self.config_file)
 
         # Allow arbitrary logging config if set
         # We do this first up so any custom logging we set up ourselves
