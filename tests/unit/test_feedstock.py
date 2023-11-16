089bcc2bcccb970ee8448d04b3a89d96d02d3a04
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2023-11-16 15:42:56 -0800

    refixture feedstock test

--- tests/unit/test_feedstock.py
+++ tests/unit/test_feedstock.py
@@ -9,29 +9,51 @@ yaml = YAML()
 
 
 @pytest.fixture(params=["recipe_object", "dict_object"])
-def meta_yaml(request):
-    return (
-        dedent(
+def tmp_feedstock(request, tmp_path_factory: pytest.TempPathFactory):
+    tmpdir = tmp_path_factory.mktemp("feedstock")
+    if request.param == "recipe_object":
+        meta_yaml = dedent(
             """\
         recipes:
           - id: aws-noaa-sea-surface-temp-whoi
             object: 'recipe:recipe'
-        """  # noqa: E501
+        """
         )
-        if request.param == "recipe_object"
-        else dedent(
+        recipe_py = dedent(
+            """\
+        class Recipe:
+          pass
+
+        recipe = Recipe()
+        """
+        )
+    elif request.param == "dict_object":
+        meta_yaml = dedent(
             """\
         recipes:
           dict_object: 'recipe:recipes'
         """
         )
-    )
+        recipe_py = dedent(
+            """\
+        class Recipe:
+          pass
 
+        recipe = {"my_recipe": Recipe()}
+        """
+        )
 
-def test_feedstock(meta_yaml, tmp_path_factory: pytest.TempPathFactory):
-    tmp = tmp_path_factory.mktemp("feedstock")
-    with open(tmp / "meta.yaml", mode="w") as f:
+    with open(tmpdir / "meta.yaml", mode="w") as f:
         f.write(meta_yaml)
+    with open(tmpdir / "recipe.py", mode="w") as f:
+        f.write(recipe_py)
 
-    f = Feedstock(feedstock_dir=tmp)
+    yield tmpdir, meta_yaml
+
+
+def test_feedstock(tmp_feedstock):
+    tmpdir, meta_yaml = tmp_feedstock
+    f = Feedstock(feedstock_dir=tmpdir)
     assert f.meta == yaml.load(meta_yaml)
+    # expanded_meta = f.get_expanded_meta()
+    # recipes = f.parse_recipes()
