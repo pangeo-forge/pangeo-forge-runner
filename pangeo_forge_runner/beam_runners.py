3559a52cafa2a443322884fcb3edc2ce9c89e9e8
ranchodeluxe <greg.corradini@gmail.com>
2024-02-15 12:52:33 -0800

    comments

--- pangeo_forge_runner/beam_runners.py
+++
@@ -1,2 +0,0 @@
-# TODO: figure out how to subclass runners and override FnApiRunner.run_pipeline
-# so it can take an optional new sys.executable path
--- pangeo_forge_runner/dtypes.py
+++ pangeo_forge_runner/dtypes.py
@@ -1,7 +1,4 @@
 # represents the apache-beam type that
-# Bakery.get_pipeline_options() should return
+# Bakery.get_pipeline_options() should return at runtime
 # https://github.com/apache/beam/blob/master/sdks/python/apache_beam/options/pipeline_options.py
-# NOTE: we don't want apache-beam to be a dependency of pangeo-forge-runner
-# but rather a dependency of the recipe that runner will install during `pangeo-forge-runner bake`
-# workflow
 ApacheBeamPipelineOptions = object
